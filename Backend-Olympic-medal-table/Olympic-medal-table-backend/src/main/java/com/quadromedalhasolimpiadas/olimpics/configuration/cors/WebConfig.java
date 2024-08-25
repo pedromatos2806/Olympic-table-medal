e8ef2e350651cdfae7724a0521c326771cca052e
Vitor Hugo <boaventurads.vitorhugo@gmail.com>
2024-08-25 18:43:56 -0300

    remoção de endereço IP desnecessáiro

--- Backend-Olympic-medal-table/Olympic-medal-table-backend/src/main/java/com/quadromedalhasolimpiadas/olimpics/configuration/cors/WebConfig.java
+++ Backend-Olympic-medal-table/Olympic-medal-table-backend/src/main/java/com/quadromedalhasolimpiadas/olimpics/configuration/cors/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-				.allowedOrigins("http://localhost:4000","http://192.168.0.104:8082/olympics-medal-table-ms")
+				.allowedOrigins("http://localhost:4000")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 				.allowedHeaders("*")
 				.allowCredentials(true);
